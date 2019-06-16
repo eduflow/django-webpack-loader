ae2af8f6d40f4d0a13330a503633b08ce58ef7b4
Tony Narlock <tony@git-pull.com>
2019-06-16 17:18:47 -0500

    setup.py: Run through black + remove download_url

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ def rel(*parts):
     '''returns the relative path to a file wrt to the current directory'''
     return os.path.abspath(os.path.join(os.path.dirname(__file__), *parts))
 
+
 README = open('README.md', 'r').read()
 
 with open(rel('flask_webpack_loader', '__init__.py')) as handler:
@@ -17,25 +18,24 @@ with open(rel('flask_webpack_loader', '__init__.py')) as handler:
 VERSION = re.findall("__version__ = '([^']+)'", INIT_PY)[0]
 
 setup(
-  name = 'flask-webpack-loader',
-  packages = ['flask_webpack_loader'],
-  version = VERSION,
-  description = 'Transparently use webpack with django',
-  long_description=README,
-  long_description_content_type="text/markdown",
-  author = 'Owais Lone',
-  author_email = 'hello@owaislone.org',
-  download_url = 'https://github.com/owais/django-webpack-loader/tarball/{0}'.format(VERSION),
-  url = 'https://github.com/owais/django-webpack-loader', # use the URL to the github repo
-  keywords = ['django', 'webpack', 'assets'], # arbitrary keywords
-  classifiers = [
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Framework :: Django',
-    'Environment :: Web Environment',
-    'License :: OSI Approved :: MIT License',
-  ],
+    name='flask-webpack-loader',
+    packages=['flask_webpack_loader'],
+    version=VERSION,
+    description='Transparently use webpack with django',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Owais Lone',
+    author_email='hello@owaislone.org',
+    url='https://github.com/owais/django-webpack-loader',  # use the URL to the github repo
+    keywords=['django', 'webpack', 'assets'],  # arbitrary keywords
+    classifiers=[
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+    ],
 )
